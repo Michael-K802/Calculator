6000982a3cff3ad2318dd61ead5fec25597c13e0
Michael-K802 <60328246+Michael-K802@users.noreply.github.com>
2020-01-26 18:16:12 -0500

    update of the calc logic javaScript file

--- calculator/calclogic.js
+++ calculator/calclogic.js
@@ -20,55 +20,145 @@ let calc = {
         clear: document.getElementById("clear"),
         equal: document.getElementById("equal"),
     },
-    mem: {
-        dataSet1: [0,0,0,1,1,2],
-        digits: 6,
-        powerBy: 6,
-        numberOfZ: 0,
-        first: '',
-        second: '',
-        stop: false,
+    mem:{
+        // write to output
+        dataSet1: [],
+        powerBy: 0,
+        digits: 0,
+        x: 0, // used in both output and solve
+        y: 0, // used in both output and solve
+        // used in operator part of the trifunc
+        isOperator: false,
         operationType: '',
+        // used for solving in the solve part of the trifunc
+        isAnswer: false,
     },
-    func: {
-        writeToOutput(input){
-            if (input){
-                calc.output.innerHTML = input;
-            } else {
-                let power = 10**calc.mem.powerBy/10;
-                let number = 0;
-                let final = 0;
-                for (let i =0; i < calc.mem.digits; i++){
-                    ind = i -1
-                    if (calc.mem.dataSet1[i] === 0 && calc.mem.dataSet1[ind]){
-                        calc.mem.numberOfZ += 1;
-                    }
-                    number = calc.mem.dataSet1[i]*power;
-                    power = power/10;
-                    final += number;
-                }
-                if (calc.mem.numberOfZ < 0){
-                    let x = final.toString;
-                    for (let i = 0; i < calc.mem.numberOfZ; i++){
-                        x = '0' + x;
-                    }
-                    calc.output.innerHTML = x
-                } else if( calc.mem.second !== '') {
-                    calc.output.innerHTML = final
-                } else {
-                    calc.output.innerHTML = final
-                }
-                calc.mem.dataSet1 = [];
-                calc.mem.digits = 0;
-                calc.mem.powerBy = 0;
-                calc.mem.numberOfZ = 0;
-                calc.mem.stop = false
+    writeToOutput(input){
+        if (input){
+            // if there are messages that shouldn't be put through the dataSet
+            calc.output.innerHTML = input;
+        } else {
+            let power = 10**calc.mem.powerBy /10;
+            let number = 0;
+            let final = 0;
+            for (let i = 0; i < calc.mem.digits; i++){
+                number = calc.mem.dataSet1[i] * power;
+                power = power/10;
+                final += number;
             }
-        },
-        tripleFunction(inputNumber, inputOpertion, sovle){
-            
+            if (calc.mem.isOperator === true || calc.mem.isAnswer === true){
+                calc.mem.y = final;
+            } else {
+                calc.mem.x = final;
+            }
+            calc.output.innerHTML = final;
         }
     },
+    trifunc(inputNumber,inputOperation,solve){
+        if (typeof inputNumber === 'number'){
+            if (calc.mem.isAnswer === true){
+                if (isOperator === false){
+                    calc.mem.isAnswer = false;
+                    calc.mem.x = '';
+                }
+            }
+            calc.mem.dataSet1.push(inputNumber);
+            if (calc.mem.isDecimal === true){
+                calc.mem.digits += 1;
+            } else {
+                calc.mem.digits += 1;
+                calc.mem.powerBy +=1;
+            }
+            calc.writeToOutput();
+        } else if (typeof inputOperation === 'string'){
+            if (inputOperation === 'add'){
+                calc.mem.operationType = inputOperation
+                calc.mem.isOperation = true;
+                calc.mem.dataSet1 = [];
+                calc.mem.powerBy = 0;
+                calc.mem.digits = 0;
+            } else if(inputOperation === 'sub'){
+                calc.mem. operationType = inputOperation
+                calc.mem.isOperation = true
+                calc.mem.dataSet1 = [];
+                calc.mem.powerBy = 0;
+                calc.mem.digits = 0;
+            } else if (inputOperation === 'multi'){
+                calc.mem.operationType = inputOperation
+                calc.mem.isOperation = true
+                calc.mem.dataSet1 = [];
+                calc.mem.powerBy = 0;
+                calc.mem.digits = 0;
+            } else if (inputOperation === 'divi'){
+                calc.mem.operationType = inputOperation
+                calc.mem.isOperation = true
+                calc.mem.dataSet1 = [];
+                calc.mem.powerBy = 0;
+                calc.mem.digits = 0;
+            } else {
+                console.log(`invalid operation: ${inputOperation}`);
+            }
+        } else if (solve === true){
+            console.log('solve ran');
+            if (calc.mem.operationType === 'add'){
+                console.log('add ran');
+                console.log(`value of x: ${calc.mem.x}`);
+                console.log(`value of y: ${calc.mem.y}`);
+                let answer = calc.mem.x + calc.mem.y;
+                console.log(answer);
+                calc.writeToOutput(answer);
+                calc.mem.isAnswer = true;
+                calc.mem.x = answer;
+                calc.mem.isOperation = false;
+                calc.mem.dataSet1 = [];
+                calc.mem.powerBy = 0;
+                calc.mem.digits = 0;
+                calc.mem.y = '';
+            } else if( calc.mem.operationType === 'sub'){
+                let answer = calc.mem.x - calc.mem.y;
+                calc.writeToOutput(answer);
+                calc.mem.isAnswer = true;
+                calc.mem.x = answer;
+                calc.mem.isOperation = false;
+                calc.mem.dataSet1 = [];
+                calc.mem.powerBy = 0;
+                calc.mem.digits = 0;
+                calc.mem.y = '';
+            } else if (calc.mem.operationType === 'multi'){
+                let answer = calc.mem.x * calc.mem.y;
+                calc.writeToOutput(answer);
+                calc.mem.isAnswer = true;
+                calc.mem.x = answer;
+                calc.mem.isOperation = false;
+                calc.mem.dataSet1 = [];
+                calc.mem.powerBy = 0;
+                calc.mem.digits = 0;
+                calc.mem.y = '';
+            } else if (calc.mem.operationType === 'divi'){
+                let answer = calc.mem.x / calc.mem.y;
+                calc.writeToOutput(answer);
+                calc.mem.isAnswer = true;
+                calc.mem.x = answer;
+                calc.mem.y = '';
+                calc.mem.isOperation = false;
+                calc.mem.dataSet1 = [];
+                calc.mem.powerBy = 0;
+                calc.mem.digits = 0;
+            } else {
+                console.log('nothing ran');
+            }
+        }
+    }
 }
-
-calc.func.writeToOutput();
+calc.keys.one.onclick = function(){
+    calc.trifunc(1);
+}
+calc.keys.two.onclick = function(){
+    calc.trifunc(2)
+}
+calc.keys.add.onclick = function(){
+    calc.trifunc('','add');
+}
+calc.keys.equal.onclick = function(){
+    calc.trifunc('',0,true);
+}
